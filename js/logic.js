4897c89dace40cb67e87fee95541ffaf2fc52a93
Asa Salcido <asasalcido14@email.arizona.edu>
2020-10-17 16:56:25 -0700

    adding question element, finish begin quiz function, start grabquestion function

--- js/logic.js
+++ js/logic.js
@@ -5,12 +5,31 @@ let timerId;
 
 
 // variables for the front end
+const questionsEl = document.getElementById("questions");
+const timerEl = document.getElementById("time");
 const beginBtn = document.getElementById("begin");
 
 
 // Begin Quiz (Function) 
 function beginQuiz() {
+
     const startPartEl = document.getElementById("start-part");
+
     startPartEl.setAttribute("class","hide")
 
+    // take question cloak off
+    questionsEl.removeAttribute("class");
+
+    //start the timer
+    timerId = setInterval(clockTick, 1000);
+
+    //display time
+    timerEl.textContent = time;
+
+    //grabber
+    grabQuestion()
+}
+
+function grabQuestion() {
+    
 }
