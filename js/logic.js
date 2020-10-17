34ab632af9272a325ae95836afe887ad31473828
Asa Salcido <asasalcido14@email.arizona.edu>
2020-10-17 16:22:53 -0700

    begin quiz function js, add comenzar to css, start-part to js

--- js/logic.js
+++ js/logic.js
@@ -3,3 +3,14 @@ let time = questions.length * 15;
 let timerId;
 // variables to monitor throughout the quiz
 
+
+// variables for the front end
+const beginBtn = document.getElementById("begin");
+
+
+// Begin Quiz (Function) 
+function beginQuiz() {
+    const startPartEl = document.getElementById("start-part");
+    startPartEl.setAttribute("class","hide")
+
+}
